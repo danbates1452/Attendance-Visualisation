52ded43211325564b8cc56f94da4afbcdb281d57
Daniel Bates <42376377+danbates1452@users.noreply.github.com>
2023-03-24 18:46:14 +0000

    started filters form

--- src/routes/filters-page.jsx
+++ src/routes/filters-page.jsx
@@ -1,11 +1,76 @@
-import { Container, Form } from "react-bootstrap";
+import { useState } from "react";
+import { Col, Container, Form, Row } from "react-bootstrap";
 import FetchAPIData from "../helper/fetchApiData";
 
 export default function FiltersPage() {
-    return(
-        <>
-            FILTER UI
+    const [table, setTable] = useState([{table: ''}])
 
-        </>
+    return(
+        <Container>
+            FILTER UI
+            <Form>
+                <Row>
+                    <Col>
+                        <Form.Group>
+                            <Form.Label htmlFor="tableSelect">Table</Form.Label>
+                            <Form.Select id="tableSelect" onChange={this.FilterOptions}>
+                                <option selected disabled hidden>Select a table</option>
+                                <option value="student">Students</option>
+                                <option value="snapshot">Snapshots</option>
+                                <option value="course">Courses</option>
+                            </Form.Select>
+                        </Form.Group>
+                    </Col>
+                    <Col></Col>
+                    <Col></Col>
+                    <Col></Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <Form.Group>
+                            <Form.Label></Form.Label>
+                        </Form.Group>
+                    </Col>
+                    <Col>
+
+                    </Col>
+                </Row>
+            </Form>
+        </Container>
     );
+}
+
+function FilterOptions(table) {
+    const all_filters = {
+        'student': [
+            'student_id',
+            'level',
+            'stage',
+            'course_code'
+        ],
+        'snapshot': [
+            'student_id',
+            'year',
+            'semester',
+            'week',
+            'insert_datetime',
+            'registration_status',
+        ],
+        'course': [
+            'code',
+            'title'
+        ]
+    }
+    let filters = (<></>);
+    if (table !== null && Object.keys(all_filters).includes(table)) {
+        //if the table name is in filters object
+        filters = all_filters[table];
+        filters = filtersToForm(filters);
+    }
+
+    
+}
+
+function filtersToForm(filter_names) {
+
 }
