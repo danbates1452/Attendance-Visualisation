281824fbec94d3d25fa4a26d8c9f4828abefc19a
Daniel Bates <42376377+danbates1452@users.noreply.github.com>
2023-03-24 16:51:04 +0000

    auto redirect to home from root

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,7 +1,17 @@
-import { Outlet } from "react-router-dom";
+import { useEffect } from "react";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
 import Navigation from "./navigation";
 
 export default function Root() {
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    useEffect(() => {
+        if (location.pathname === '/') {
+            navigate('/home') //auto-redirect to /home from /
+        }
+    }, [location, navigate])
+
     return (
         <>
             <Navigation/>
