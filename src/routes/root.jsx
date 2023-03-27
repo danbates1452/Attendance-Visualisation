3b03733e7cfbaeece75f89acc0b6762d0a134cfc
Daniel Bates <42376377+danbates1452@users.noreply.github.com>
2023-03-28 00:12:43 +0100

    updated navbar

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -27,18 +27,27 @@ function Navigation() {
     return (
         <Navbar bg="dark" variant="dark" expand="lg">
           <Container>
-            <Navbar.Brand as={Link} to="/home"><strong >Attendance Visualisation</strong></Navbar.Brand>
+            <Navbar.Brand as={Link} to="/home">
+              <img
+              alt=""
+              src="/logo512.png"
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+              />{' '}
+              <strong >Attendance Visualisation</strong>
+            </Navbar.Brand>
             <Navbar.Toggle aria-controls="application-navbar"></Navbar.Toggle>
             <Navbar.Collapse id="application-navbar">
               <Nav className="me-auto">
-                <Nav.Link as={Link} to="/home">Home</Nav.Link>
-                <Nav.Link as={Link} to="/filters">Filters</Nav.Link>
+                <Nav.Link eventKey="link-1" as={Link} to="/home">Home</Nav.Link>
+                <Nav.Link eventKey="link-2" as={Link} to="/filters">Filters</Nav.Link>
                 <RoleWrapper role={getCurrentUserRole()} allowedRoles={'admin'}>
-                  <Nav.Link as={Link} to="/upload">Upload Data</Nav.Link>
-                  <Nav.Link as={Link} to="/users">User Management</Nav.Link>
-                  <Nav.Link as={Link} to="/data">Data Management</Nav.Link>
+                  <Nav.Link eventKey="link-3" as={Link} to="/upload">Upload Data</Nav.Link>
+                  <Nav.Link eventKey="link-4" as={Link} to="/users">User Management</Nav.Link>
+                  <Nav.Link eventKey="link-5"as={Link} to="/data">Data Management</Nav.Link>
                 </RoleWrapper>
-                <Nav.Link as={Link} to="/login">Login</Nav.Link>
+                <Nav.Link eventKey="link-6" as={Link} to="/login">Login</Nav.Link>
               </Nav>
             </Navbar.Collapse>
           </Container>
