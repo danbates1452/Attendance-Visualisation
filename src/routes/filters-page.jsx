15e1e5fecc447d45cfb5cbdc29f86f201ab15467
Daniel Bates <42376377+danbates1452@users.noreply.github.com>
2023-04-03 17:15:15 +0100

    got filter page working (for tables)

--- src/routes/filters-page.jsx
+++ src/routes/filters-page.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { Button, Col, Container, Form, Row, Table } from "react-bootstrap";
 import FetchAPIData from "../helper/fetchApiData";
 import Select from 'react-select';
+import {v4 as uuidv4} from 'uuid';
 
 export default function FiltersPage() {
     const [filters, setFilters] = useState([<TableFilters key="filter" tableName="EMPTY"/>]);
@@ -52,31 +53,26 @@ function objectArrayToStringArray(objectArray) {
 }
 
 function View({type, data={}}) {
-    if (data === {} || data === undefined) {type = ''} //clear type and cause switch to default if empty data
+    if (data === {} || data === undefined || data[0] === undefined) {type = ''} //clear type and cause switch to default if empty data
     
     switch (type) {
         case 'line':
             return (<></>); //TODO: complete this chart generation
         case 'table':
-            console.log(data);
-
-            /*
-            
-            */
             return (
                 <Row><Table>
                 <thead>
                 <tr key={'header'}>
                 {Object.keys(data[0]).map((key) => (
-                        <th>{key}</th>
+                        <th key={key}>{key}</th>
                     ))}
                 </tr>
                 </thead>
                 <tbody>
                 {data.map((item) => (
-                <tr key={item.student_id}>
+                <tr key={uuidv4()}>
                     {Object.values(item).map((value) => (
-                        <td>{value}</td>
+                        <td key={uuidv4()}>{value}</td>
                     ))}
                 </tr>
                 ))}
@@ -84,33 +80,72 @@ function View({type, data={}}) {
                 </Table></Row>
             );
         default: // return nothing
-            console.log('defaulted');
             return ('');
     }
 }
 
-function StudentFilter() {
-    const [viewType, setViewType] = useState('');
-    const [viewData, setViewData] = useState({});
+function TableFilters({tableName}) {
+    //student view
+    const [studentViewType, setStudentViewType] = useState('');
+    const [studentViewData, setStudentViewData] = useState({});
+    //snapshot view
+    const [snapshotViewType, setSnapshotViewType] = useState('');
+    const [snapshotViewData, setSnapshotViewData] = useState({});
+    //course view
+    const [courseViewType, setCourseViewType] = useState('');
+    const [courseViewData, setCourseViewData] = useState({});
 
-    let fetchStudentOptions = FetchAPIData('/api/filter_options/student');
-    
-    let studentOptions = optionArrayToObjectArray(fetchStudentOptions['student_id']);
-    let courseOptions = optionArrayToObjectArray(fetchStudentOptions['course_code']);
-    let stageOptions = optionArrayToObjectArray(fetchStudentOptions['stage']);
-    let levelOptions = optionArrayToObjectArray(fetchStudentOptions['level']);
-
-    const [student_id, setStudent_id] = useState(""); //form states
+    //student states
+    const [student_id, setStudent_id] = useState(""); //shared between student and snapshot
     const [course, setCourse] = useState("");
     const [level, setLevel] = useState("");
     const [stage, setStage] = useState("");
+    //snapshot states
+    const [registration_status, setRegistration_status] = useState("");
+    const [year, setYear] = useState("");
+    const [semester, setSemester] = useState("");
+    const [week, setWeek] = useState("");
+    //course states
+    const [code, setCode] = useState("");
+    const [title, setTitle] = useState("");
 
-    const handleStudent_idChange = (selectedOption) => {setStudent_id(selectedOption)};
+    //student selector handlers
+    const handleStudent_idChange = (selectedOption) => {setStudent_id(selectedOption)}; //shared by student and snapshot
     const handleCourseChange = (selectedOption) => {setCourse(selectedOption)};
     const handleLevelChange = (selectedOption) => {setLevel(selectedOption)};
     const handleStageChange = (selectedOption) => {setStage(selectedOption)};
+    //snapshot selector handlers
+    const handleRegistration_statusChange = (selectedOption) => {setRegistration_status(selectedOption)};
+    const handleYearChange = (selectedOption) => {setYear(selectedOption)};
+    const handleSemesterChange = (selectedOption) => {setSemester(selectedOption)};
+    const handleWeekChange = (selectedOption) => {setWeek(selectedOption)};
+    //course selector handlers
+    const handleCodeChange = (selectedOption) => {setCode(selectedOption)};
+    const handleTitleChange = (selectedOption) => {setTitle(selectedOption)};
 
-    const handleSubmit = (event) => {
+    async function fetchData(url, tableName) {
+        const response = await fetch(url);
+        const data = await response.json();
+        switch (tableName) {
+            case 'student':
+                setStudentViewData(Object.values(data));
+                setStudentViewType('table');
+                break;
+            case 'snapshot':
+                setSnapshotViewData(Object.values(data));
+                setSnapshotViewType('table');
+                break;
+            case 'course':
+                setCourseViewData(Object.values(data));
+                setCourseViewType('table');
+                break;
+            default:
+                //do nothing - shouldn't reach this
+                break;
+        }
+    }
+
+    const handleStudentSubmit = (event) => {
         event.preventDefault();
 
         let params = new URLSearchParams();
@@ -121,111 +156,139 @@ function StudentFilter() {
 
         const url = '/api/filter/student?' + params.toString();
 
-        async function fetchData() {
-            const response = await fetch(url);
-            const data = await response.json();
-            setViewData(Object.values(data));
-            setViewType('table');
-        }
-        fetchData();
+        fetchData(url, 'student');
     };
 
-    return (
-        <>
-            <Form key={'StudentForm'} onSubmit={handleSubmit} method="get">
-                <Row>
-                    <Form.Group as={Col}>
-                        <Form.Label>Student ID</Form.Label>
-                        <Select options={studentOptions} onChange={handleStudent_idChange} value={student_id} isMulti isClearable isSearchable/>
+    const handleSnapshotSubmit = (event) => {
+        event.preventDefault();
 
-                        <Form.Label>Course</Form.Label>
-                        <Select options={courseOptions} onChange={handleCourseChange} value={course} isMulti isClearable isSearchable/>
-                    </Form.Group>
+        let params = new URLSearchParams();
+        objectArrayToStringArray(student_id).map((item) => params.append('student_id', item));
+        objectArrayToStringArray(registration_status).map((item) => params.append('registration_status', item));
+        objectArrayToStringArray(year).map((item) => params.append('year', item));
+        objectArrayToStringArray(semester).map((item) => params.append('semester', item));
+        objectArrayToStringArray(week).map((item) => params.append('week', item));
 
-                    <Form.Group as={Col}>
-                        <Form.Label>Level</Form.Label>
-                        <Select options={levelOptions} onChange={handleLevelChange} value={level} isMulti isClearable isSearchable defaultValue={levelOptions}/>
+        const url = '/api/filter/snapshot?' + params.toString();
 
-                        <Form.Label>Stage</Form.Label>
-                        <Select options={stageOptions} onChange={handleStageChange} value={stage} isMulti isClearable isSearchable defaultValue={stageOptions}/>
+        fetchData(url, 'snapshot');
+    };
 
-                        <Button variant="primary" type="submit">
-                            Filter
-                        </Button>
-                    </Form.Group>
-                </Row>
-            </Form>
-            <View key="StudentView" type={viewType} data={viewData}/>
-        </>
-    );
-}
+    const handleCourseSubmit = (event) => {
+        event.preventDefault();
 
-function snapshotFilter() {
-    let fetchSnapshotOptions = FetchAPIData('/api/filter_options/snapshot');
-    
-    let studentOptions = optionArrayToObjectArray(fetchSnapshotOptions['student_id']);
-    let registrationOptions = optionArrayToObjectArray(fetchSnapshotOptions['registration_status']);
-    let yearOptions = optionArrayToObjectArray(fetchSnapshotOptions['year']);
-    let semesterOptions = optionArrayToObjectArray(fetchSnapshotOptions['semester']);
-    let weekOptions = optionArrayToObjectArray(fetchSnapshotOptions['week']);
+        let params = new URLSearchParams();
+        objectArrayToStringArray(code).map((item) => params.append('code', item));
+        objectArrayToStringArray(title).map((item) => params.append('title', item));
+        
+        const url = '/api/filter/course?' + params.toString();
 
-    return (
-        <>
-            <Row>
-                <Form.Group as={Col}>
-                    <Form.Label>Student ID</Form.Label>
-                    <Select options={studentOptions}/>
+        fetchData(url, 'course');
+    };
 
-                    <Form.Label>Registration Status</Form.Label>
-                    <Select options={registrationOptions}/>
-                </Form.Group>
-
-                <Form.Group as={Col}>
-                    <Form.Label>Year</Form.Label>
-                    <Select options={yearOptions}/>
-
-                    <Form.Label>Semester</Form.Label>
-                    <Select options={semesterOptions}/>
-
-                    <Form.Label>Week</Form.Label>
-                    <Select options={weekOptions}/>
-                </Form.Group>
-            </Row>
-        </>
-    );
-}
-
-function courseFilter() {
-    let fetchCourseOptions = FetchAPIData('/api/filter_options/course');
-
-    let codeOptions = optionArrayToObjectArray(fetchCourseOptions['code']);
-    let titleOptions = optionArrayToObjectArray(fetchCourseOptions['title']);
-
-    return (
-        <>
-            <Row>
-                <Form.Group as={Col}>
-                    <Form.Label>Course Code</Form.Label>
-                    <Select options={codeOptions}/>
-                </Form.Group>
-
-                <Form.Group as={Col}>
-                    <Form.Label>Course Title</Form.Label>
-                    <Select options={titleOptions}/>
-                </Form.Group>
-            </Row>
-        </>
-    );
-}
-
-function TableFilters({tableName}) {
+    let studentOptions;
     switch (tableName) {
         case 'student': //TODO: pull all course codes and titles (display as "CODE - TITLE") for selection
-            return StudentFilter();
+            let fetchStudentOptions = FetchAPIData('/api/filter_options/student');
+    
+            studentOptions = optionArrayToObjectArray(fetchStudentOptions['student_id']);
+            let courseOptions = optionArrayToObjectArray(fetchStudentOptions['course_code']);
+            let stageOptions = optionArrayToObjectArray(fetchStudentOptions['stage']);
+            let levelOptions = optionArrayToObjectArray(fetchStudentOptions['level']);
+        
+            return (
+                <>
+                    <Form key={'StudentForm'} onSubmit={handleStudentSubmit}>
+                        <Row>
+                            <Form.Group as={Col}>
+                                <Form.Label>Student ID</Form.Label>
+                                <Select options={studentOptions} onChange={handleStudent_idChange} value={student_id} isMulti isClearable isSearchable/>
+    
+                                <Form.Label>Course</Form.Label>
+                                <Select options={courseOptions} onChange={handleCourseChange} value={course} isMulti isClearable isSearchable/>
+                            </Form.Group>
+    
+                            <Form.Group as={Col}>
+                                <Form.Label>Level</Form.Label>
+                                <Select options={levelOptions} onChange={handleLevelChange} value={level} isMulti isClearable isSearchable defaultValue={levelOptions}/>
+    
+                                <Form.Label>Stage</Form.Label>
+                                <Select options={stageOptions} onChange={handleStageChange} value={stage} isMulti isClearable isSearchable defaultValue={stageOptions}/>
+                            </Form.Group>
+                        </Row>
+                        <Row className='p-2 ml-3 mr-3'>
+                            <Button variant="primary" type="submit">Filter</Button>
+                        </Row>
+                    </Form>
+                    <View key="StudentView" type={studentViewType} data={studentViewData}/>
+                </>
+            );
         case 'snapshot':
-            return snapshotFilter();
-        case 'course':
-            return courseFilter();
+            let fetchSnapshotOptions = FetchAPIData('/api/filter_options/snapshot');
+    
+            studentOptions = optionArrayToObjectArray(fetchSnapshotOptions['student_id']);
+            let registrationOptions = optionArrayToObjectArray(fetchSnapshotOptions['registration_status']);
+            let yearOptions = optionArrayToObjectArray(fetchSnapshotOptions['year']);
+            let semesterOptions = optionArrayToObjectArray(fetchSnapshotOptions['semester']);
+            let weekOptions = optionArrayToObjectArray(fetchSnapshotOptions['week']);
+
+            return (
+                <>
+                    <Form key={'SnapshotForm'} onSubmit={handleSnapshotSubmit}>
+                        <Row>
+                            <Form.Group as={Col}>
+                                <Form.Label>Student ID</Form.Label>
+                                <Select options={studentOptions} onChange={handleStudent_idChange} isMulti isClearable isSearchable/>
+
+                                <Form.Label>Registration Status</Form.Label>
+                                <Select options={registrationOptions}  onChange={handleRegistration_statusChange} isMulti isClearable isSearchable/>
+                            </Form.Group>
+
+                            <Form.Group as={Col}>
+                                <Form.Label>Year</Form.Label>
+                                <Select options={yearOptions}  onChange={handleYearChange} isMulti isClearable isSearchable/>
+
+                                <Form.Label>Semester</Form.Label>
+                                <Select options={semesterOptions}  onChange={handleSemesterChange} isMulti isClearable isSearchable/>
+
+                                <Form.Label>Week</Form.Label>
+                                <Select options={weekOptions}  onChange={handleWeekChange} isMulti isClearable isSearchable/>
+                            </Form.Group>
+                        </Row>
+                        <Row className='p-2 ml-3 mr-3'>
+                            <Button variant="primary" type="submit">Filter</Button>
+                        </Row>
+                    </Form>
+                    <View key="SnapshotView" type={snapshotViewType} data={snapshotViewData}/>
+                </>
+            );
+                case 'course':
+                    let fetchCourseOptions = FetchAPIData('/api/filter_options/course');
+
+                    let codeOptions = optionArrayToObjectArray(fetchCourseOptions['code']);
+                    let titleOptions = optionArrayToObjectArray(fetchCourseOptions['title']);
+                
+                    return (
+                        <>
+                            <Form key={'CourseForm'} onSubmit={handleCourseSubmit}>
+                                <Row>
+                                    <Form.Group as={Col}>
+                                        <Form.Label>Course Code</Form.Label>
+                                        <Select options={codeOptions}  onChange={handleCodeChange} isMulti isClearable isSearchable/>
+                                    </Form.Group>
+                    
+                                    <Form.Group as={Col}>
+                                        <Form.Label>Course Title</Form.Label>
+                                        <Select options={titleOptions}  onChange={handleTitleChange} isMulti isClearable isSearchable/>
+                                    </Form.Group>
+                                </Row>
+                                <Row className='p-2 ml-3 mr-3'>
+                                    <Button variant="primary" type="submit">Filter</Button>
+                                </Row>
+                            </Form>
+                            <View key="CourseView" type={courseViewType} data={courseViewData}/>
+                        </>
+                    );
         default:
             return '';
     }
